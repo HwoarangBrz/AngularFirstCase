ca8a3968125566d90df4bde93abccbb65a36d304
Ricardo Jorge <ricardo.s.jorge@hotmail.com>
2019-11-20 21:13:26 -0300

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanComponent } from './plan/plan.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plan/plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan',
+  templateUrl: './plan.component.html',
+  styleUrls: []
+})
+export class PlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
